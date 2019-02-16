34120fc3d3f842f40b04effa5ec7b460c502164f
cesarvr <cesarv01@yahoo.com>
2019-02-16 10:00:09 +0000

    adding win32 support

--- hello.cc
+++ hello.cc
@@ -1,14 +1,38 @@
 #include <nan.h>
 #include <iostream>
-
-// getpass()
-#include <pwd.h>
-#include <unistd.h>
-
-// sleep
-#include <unistd.h>
-
 using namespace std;
+
+#if defined( __linux__) || defined(__APPLE__)
+    //linux code goes here
+    #include <pwd.h>
+    #include <chrono>
+    #include <thread>
+#elif _WIN32
+    // windows code goes here
+    #include <chrono>
+    #include <thread>
+#endif
+
+string readFromKeyboard(string message) {
+  string input;
+
+  cout << message;
+  cin >> input;
+
+  return input;
+}
+
+string readOfuscatedFromKeyboard(string message){
+
+  #if defined( __linux__) || defined(__APPLE__)
+    char *psw = getpass(message.c_str());
+    string output(psw);
+  #elif _WIN32
+    string output = readFromKeyboard(message);
+  #endif
+
+  return output;
+}
 
 template <typename Info>
 bool InvalidArgs(Info& info, int argc){
@@ -38,11 +62,8 @@ void GetInputBlocking(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     if(InvalidArgs(info, 1)) return;
 
     auto message = Standard::ToString( info[0]->ToString() );
-    string input;
 
-    cout << message;
-    cin >> input;
-
+    auto input = readFromKeyboard(message);
 
     info.GetReturnValue().Set(Nan::New(input.c_str()).ToLocalChecked());
 }
@@ -52,9 +73,8 @@ void GetHiddenInputBlocking(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     if(InvalidArgs(info, 1)) return;
 
     auto message = Standard::ToString( info[0]->ToString() );
-    char *psw = getpass(message.c_str());
+    auto output  = readOfuscatedFromKeyboard(message);
 
-    string output(psw);
     info.GetReturnValue().Set(Nan::New(output.c_str()).ToLocalChecked());
 }
 
@@ -64,9 +84,9 @@ void Sleep(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 
     if(info[0]->IsNumber()) {
         sleep_timer = info[0]->Int32Value();
-    } 
+    }
 
-    sleep(sleep_timer);
+    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_timer));
     info.GetReturnValue().Set(Nan::New(sleep_timer));
 }
 
@@ -82,8 +102,9 @@ void Init(v8::Local<v8::Object> exports) {
     exports->Set(Nan::New("input").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(GetInputBlocking)->GetFunction());
 
-    exports->Set(Nan::New("password").ToLocalChecked(),
+      exports->Set(Nan::New("password").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(GetHiddenInputBlocking)->GetFunction());
+
 
     exports->Set(Nan::New("sleep").ToLocalChecked(),
                  Nan::New<v8::FunctionTemplate>(Sleep)->GetFunction());
